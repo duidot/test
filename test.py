fdd01a9694841d902ee413f599d9f2bcf7eec0f2
duidot <d.dottavio@iisvoltapescara.edu.it>
2023-03-07 23:05:55 +0100

    aggiunto test1.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-import numpy as np
 
-print("Hellooo")
-=======
-print("Hellooo")
->>>>>>> a4f69e40400201c01cdbeacf42c45e40dd67fb49
+for k in range(20):
+    print(k, (k*300+1)/17)
+        
+
